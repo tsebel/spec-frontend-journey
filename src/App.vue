a3bde57c5dd9f40ba078a7a8ba429da48c9f529b
Tsebel Tsigemeskel <tmtsebel@gmail.com>
2026-02-04 12:15:24 +0300

    day2: v-model(input binding) example

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,18 @@
-<template><h1>Hello SPEC Developers </h1></template>
+<script setup>
+import { ref } from 'vue'
+const name = ref('')
+</script>
+
+<template>
+<div>
+  <h1>Hello SPEC Developers</h1>
+</div>
+<br>
+<div>
+  <input v-model="name" placeholder="Type your name">
+</div>
+<br>
+<div>
+  <p>Hello {{ name }}</p>
+</div>
+</template>
